29de324adf2bec390b518222eac42220907f2faa
juliocvd9891 <julio.vargas.condor@gmail.com>
2025-04-20 12:18:35 -0600

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/juliocvd9891/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('juliocvd9891')
+            node / gitConfigEmail('julio.vargas.condor@gmail.com')
         }
     }
     triggers {
